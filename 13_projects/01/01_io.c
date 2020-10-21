64eb7ebb1515594dfbba16b06c76261fc96b48ef
aymkh <aymen4real@gmail.com>
2020-10-21 12:01:45 +0100

    project 01 IO

--- 13_projects/01/01_io.c
+++ 13_projects/01/01_io.c
@@ -3,20 +3,22 @@
 int main(){
 
     int givenSeconds;
-
     int hours;
     int min;
-
-
-
-    printf("Type in the number of seconds : ");
-    scanf("%d", &givenSeconds);
-
-    hours = givenSeconds/3600;
+    int sec;
     
-
-
-    printf("H : %d", hours);
+    // Getting the user input
+    printf("Type in the number of Seconds : ");
+    scanf("%d", &givenSeconds);
+    
+	// calculating the Hours, Minutes & Seconds    
+    hours = givenSeconds / 3600;
+    min = (givenSeconds % 3600 ) /60;
+    sec = givenSeconds % 60;
+    
+    //Displaying the result
+    printf("H : %d, M : %d, S : %d", hours, min, sec);
+    
 
     return 0;
 }
