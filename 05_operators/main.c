c2fa4278784e571f4287bd0502fc8b3efc45bf5e
aymkh <aymen4real@gmail.com>
2020-10-21 12:01:30 +0100

    opertators

---
+++ 05_operators/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 5;
+    int y = 2;
+
+    printf("Addition : %d \n", x+y);
+    printf("Substruction : %d \n", x-y);
+    printf("Multiplication : %d \n", x*y);
+    printf("Division : %d \n", x/y);
+    printf("MOD : %d \n", x%y);
+
+    return 0;
+}
