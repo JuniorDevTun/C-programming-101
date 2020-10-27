8038d18c0d665242842ea100b26cbb14ecab173d
aymkh <aymen4real@gmail.com>
2020-10-27 12:17:02 +0100

    Loops 1+2

---
+++ 07_loops/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(){
+
+    int i;
+    for ( i = 0; i < 10; i++){
+
+        printf("I : %d \n", i);
+
+    }
+
+    int y = 0;
+    while ( y != 10) {
+        printf("Y : %d \n", y);
+        y++;
+    }
+
+    
+}
